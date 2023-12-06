21d76cfe57425c32cc42a37d25288197a19c9c3f
baophucish <phuc.hoangbao304@gmail.com>
2023-12-06 15:05:02 +0700

    6/12/2023

---
+++ app/src/main/java/com/nguyenhoangbaophuc/newsapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nguyenhoangbaophuc.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    // apikey: 325fd10f6d15488dbe57311d5f12077e
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
